85309ba2e0c78495d57209262d9669f209da8e04
howsohazard <143410553+howsohazard@users.noreply.github.com>
2023-11-07 11:36:26 -0500

    18208: Adds null option to set_digits to skip setting the digit (#32)

--- src/Amalgam/interpreter/InterpreterOpcodesMath.cpp
+++ src/Amalgam/interpreter/InterpreterOpcodesMath.cpp
@@ -380,6 +380,14 @@ EvaluableNodeReference Interpreter::InterpretNode_ENT_SET_DIGITS(EvaluableNode *
 
 				if(cur_digit_index >= digits_ocn.size())
 					break;
+
+				//skip nulls
+				if(EvaluableNode::IsNull(digits_ocn[cur_digit_index]))
+				{
+					cur_digit_index++;
+					continue;
+				}
+
 				double new_digit = EvaluableNode::ToNumber(digits_ocn[cur_digit_index++]);
 
 				result_value -= value_digit * place_value;
@@ -405,6 +413,14 @@ EvaluableNodeReference Interpreter::InterpretNode_ENT_SET_DIGITS(EvaluableNode *
 				{
 					if(cur_digit_index >= digits_ocn.size())
 						break;
+
+					//skip nulls
+					if(EvaluableNode::IsNull(digits_ocn[cur_digit_index]))
+					{
+						cur_digit_index++;
+						continue;
+					}
+
 					double new_digit = EvaluableNode::ToNumber(digits_ocn[cur_digit_index++]);
 
 					result_value -= value_digit * place_value;

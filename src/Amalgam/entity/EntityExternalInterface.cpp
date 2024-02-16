9c1a8ba65616e886b3ae67c39c1c8511254ada45
J. Caleb Wherry <337871+calebwherry@users.noreply.github.com>
2024-02-15 19:30:19 -0500

    Bug fix.

--- src/Amalgam/entity/EntityExternalInterface.cpp
+++ src/Amalgam/entity/EntityExternalInterface.cpp
@@ -24,7 +24,7 @@ LoadEntityStatus::LoadEntityStatus()
 
 void LoadEntityStatus::Set(bool loaded_in, std::string message_in, std::string version_in)
 {
-	loaded = loaded;
+	loaded = loaded_in;
 	message = std::move(message_in);
 	version = std::move(version_in);
 }

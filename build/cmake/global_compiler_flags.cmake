91ca46e663ef4e3e1498a9da7aff4f7407755352
apbassett <43486400+apbassett@users.noreply.github.com>
2024-07-02 14:45:18 -0400

    Add to Apple Clang

--- build/cmake/global_compiler_flags.cmake
+++ build/cmake/global_compiler_flags.cmake
@@ -88,7 +88,7 @@ elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "AppleClang")
         message(WARNING "AppleClang version '${CMAKE_CXX_COMPILER_VERSION}' < 14, usage is not officially supported")
     endif()
 
-    string(APPEND CMAKE_CXX_FLAGS " -fPIC -fno-strict-aliasing -Wall -Wno-unknown-pragmas -Werror")
+    string(APPEND CMAKE_CXX_FLAGS " -fPIC -fno-strict-aliasing -fno-exceptions -Wall -Wno-unknown-pragmas -Werror")
     # TODO 1599: Additional warnings that are fairly strict, not enabled right now
     #string(APPEND CMAKE_CXX_FLAGS " -Wpedantic -Wextra -Wabi")
 

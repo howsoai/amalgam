d12e33481b47758b4e82427a2b7b6b52dbd2d7e7
Christopher J. Hazard, PhD <143410553+howsohazard@users.noreply.github.com>
2024-09-02 12:33:53 -0400

    21453: Improves consistency of use of condition variables in ThreadPool

--- src/Amalgam/ThreadPool.h
+++ src/Amalgam/ThreadPool.h
@@ -82,6 +82,7 @@ public:
 		//compute and compare the current thread pool size to that which is needed
 		int32_t cur_thread_pool_size = static_cast<int32_t>(threads.size());
 		int32_t needed_thread_pool_size = (numReservedThreads + numThreadsToTransitionToReserved) + num_threads_needed;
+		size_t num_threads_added = 0;
 		if(cur_thread_pool_size < needed_thread_pool_size)
 		{
 			//if there are reserved threads, use them, otherwise create a new thread
@@ -91,15 +92,17 @@ public:
 			}
 			else
 			{
+				num_threads_added = needed_thread_pool_size - cur_thread_pool_size;
 				for(; cur_thread_pool_size < needed_thread_pool_size; cur_thread_pool_size++)
-				{
 					AddNewThread();
-					waitForTask.notify_one();
-				}
 			}
 		}
 
 		numActiveThreads--;
+
+		lock.unlock();
+		for(size_t i = 0; i < num_threads_added; i++)
+			waitForTask.notify_one();
 	}
 
 	//changes the current thread state from waiting to active
@@ -115,6 +118,8 @@ public:
 		if(numActiveThreads > maxNumActiveThreads)
 		{
 			numThreadsToTransitionToReserved++;
+
+			lock.unlock();
 			waitForTask.notify_one();
 		}
 	}

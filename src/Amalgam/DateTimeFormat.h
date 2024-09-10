20ae1e547a12764b9c6f50cdc8cbee2f4e949a82
Christopher J. Hazard, PhD <143410553+howsohazard@users.noreply.github.com>
2024-09-10 14:40:27 -0400

    21513: Adds time capabilities to format opcode

--- src/Amalgam/DateTimeFormat.h
+++ src/Amalgam/DateTimeFormat.h
@@ -9,8 +9,14 @@
 std::string SetTimeZoneDatabasePath(std::string path = "");
 
 //parses datetime based on format and locale and returns the number of seconds from "Epoch" (January 1, 1970)
-double GetNumSecondsSinceEpochFromDateTimeString(const std::string &datetime, std::string format, std::string locale, std::string timezone);
+double GetNumSecondsSinceEpochFromDateTimeString(const std::string &datetime_str, std::string format, std::string locale, std::string timezone);
 
-//transforms timepoint into the datetime string specified by format and locale based on "Epoch" (January 1, 1970)
+//transforms seconds_since_epoch into the datetime string specified by format and locale based on "Epoch" (January 1, 1970)
 // positive and negative values of num_secs_from_epoc are allowed
-std::string GetDateTimeStringFromNumSecondsSinceEpoch(double num_secs_from_epoch, std::string format, const std::string &locale, const std::string &timezone);
+std::string GetDateTimeStringFromNumSecondsSinceEpoch(double seconds_since_epoch, std::string format, const std::string &locale, const std::string &timezone);
+
+//parses time_str based on format and locale and returns the number of seconds since midnight
+double GetNumSecondsSinceMidnight(const std::string &time_str, std::string format, std::string locale);
+
+//transfroms seconds_since_midnight into a string representing the time of day
+std::string GetTimeStringFromNumSecondsSinceMidnight(double seconds_since_midnight, std::string format, std::string locale);

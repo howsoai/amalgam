cb14b570c86b4b09e227abd5406a40418f09d562
Christopher J. Hazard, PhD <143410553+howsohazard@users.noreply.github.com>
2024-09-23 09:21:53 -0400

    21636: Fixes possible crash when zipping an empty list

--- src/Amalgam/interpreter/InterpreterOpcodesTransformations.cpp
+++ src/Amalgam/interpreter/InterpreterOpcodesTransformations.cpp
@@ -1669,18 +1669,16 @@ EvaluableNodeReference Interpreter::InterpretNode_ENT_ZIP(EvaluableNode *en, boo
 
 		//get value
 		EvaluableNode *value = nullptr;
-		if(value_list != nullptr)
+		if(EvaluableNode::IsOrderedArray(value_list))
 		{
-			if(value_list->IsOrderedArray() && i < value_list->GetOrderedChildNodes().size())
-			{
+			if(i < value_list->GetOrderedChildNodesReference().size())
 				value = value_list->GetOrderedChildNodes()[i];
-			}
-			else //not a list, so just use the value itself
-			{
-				value = value_list;
-				//reusing the value, so can't be cycle free in the result
-				result->SetNeedCycleCheck(true);
-			}
+		}
+		else //not a list, so just use the value itself
+		{
+			value = value_list;
+			//reusing the value, so can't be cycle free in the result
+			result->SetNeedCycleCheck(true);
 		}
 
 		//if no function, then just put value into the appropriate slot for the index

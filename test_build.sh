91b5bc1c0a9f265194684e43ad23c5cc5d2a32aa
apbassett <43486400+apbassett@users.noreply.github.com>
2024-10-04 13:27:23 -0400

    Test non-cmake script

---
+++ test_build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+ 
+CC=g++-13
+ 
+INCLUDE_DIRS="-I${PWD}/src/3rd_party -I${PWD}/src/Amalgam -I${PWD}/src/Amalgam/entity -I${PWD}/src/Amalgam/evaluablenode -I${PWD}/src/Amalgam/importexport -I${PWD}/src/Amalgam/interpreter -I${PWD}/src/Amalgam/rand -I${PWD}/src/Amalgam/string"
+ 
+SOURCES=$(find ${PWD}/src -name '*.cpp')
+ 
+THIRD_PARTY_FLAGS="-DUSE_OS_TZDB=1 -Wno-deprecated-declarations -fPIC -fno-strict-aliasing -Wall -Wno-unknown-pragmas -Werror"
+ 
+# debug flags
+COMPILER_FLAGS="-g -O0"
+#release flags
+#COMPILER_FLAGS="-g -O3 -DMULTITHREAD_SUPPORT"
+ 
+LINKER_FLAGS="-lpthread"
+ 
+${CC} ${THIRD_PARTY_FLAGS} ${COMPILER_FLAGS} ${SOURCES} ${INCLUDE_DIRS} -o amalgam ${LINKER_FLAGS}
+ 
+ls

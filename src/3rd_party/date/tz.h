28a5501d3db71ba951d23ed2512419ea1f4f2794
Matt Fulp <8397318+fulpm@users.noreply.github.com>
2024-10-18 15:20:16 -0400

    tz header

--- src/3rd_party/date/tz.h
+++ src/3rd_party/date/tz.h
@@ -1206,11 +1206,11 @@ struct tzdb
 #endif  // defined(_MSC_VER) && (_MSC_VER < 1900)
 
 #if HAS_STRING_VIEW
-    DATE_API const time_zone* locate_zone(std::string_view tz_name) const;
+    const time_zone* locate_zone(std::string_view tz_name) const;
 #else
-    DATE_API const time_zone* locate_zone(const std::string& tz_name) const;
+    const time_zone* locate_zone(const std::string& tz_name) const;
 #endif
-    DATE_API const time_zone* current_zone() const;
+    const time_zone* current_zone() const;
 };
 
 using TZ_DB = tzdb;
@@ -1225,9 +1225,9 @@ class tzdb_list
     std::atomic<tzdb*> head_{nullptr};
 
 public:
-    DATE_API ~tzdb_list();
+    ~tzdb_list();
     tzdb_list() = default;
-    DATE_API tzdb_list(tzdb_list&& x) NOEXCEPT;
+    tzdb_list(tzdb_list&& x) NOEXCEPT;
 
     const tzdb& front() const NOEXCEPT {return *head_;}
           tzdb& front()       NOEXCEPT {return *head_;}
@@ -1240,7 +1240,7 @@ public:
     const_iterator cbegin() const NOEXCEPT;
     const_iterator cend() const NOEXCEPT;
 
-    DATE_API const_iterator erase_after(const_iterator p) NOEXCEPT;
+    const_iterator erase_after(const_iterator p) NOEXCEPT;
 
     struct undocumented_helper;
 private:

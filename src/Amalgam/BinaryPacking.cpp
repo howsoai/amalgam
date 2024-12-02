13e8fe594cf8f85da74df8ef0825dce1753bcde8
Christopher J. Hazard, PhD <143410553+howsohazard@users.noreply.github.com>
2024-12-01 22:04:14 -0500

    21364: Rearranges some code

--- src/Amalgam/BinaryPacking.cpp
+++ src/Amalgam/BinaryPacking.cpp
@@ -54,284 +54,151 @@ size_t ParseCompactIndexToIndexAndAdvance(BinaryData &bd, size_t &bd_offset)
 	return index;
 }
 
-//Huffman Encoding implementation for compressing and decompressing data
-template <typename value_type>
-class HuffmanTree
+StringCodec::StringCodec(std::array<uint8_t, NUM_UINT8_VALUES> &byte_frequencies)
 {
-public:
-	constexpr HuffmanTree(value_type value, size_t value_frequency, size_t node_index,
-		HuffmanTree<value_type> *left = nullptr, HuffmanTree<value_type> *right = nullptr)
-		: value(value), valueFrequency(value_frequency), nodeIndex(node_index), left(left), right(right)
-	{	}
+	//build the huffman_tree based on the byte frequencies
+	huffmanTree = HuffmanTree<uint8_t>::BuildTreeFromValueFrequencies(byte_frequencies);
+}
 
-	~HuffmanTree()
+StringCodec::~StringCodec()
+{
+	if(huffmanTree != nullptr)
+		delete huffmanTree;
+}
+
+BinaryData StringCodec::EncodeString(std::string &uncompressed_data)
+{
+	//build lookup table from huffman_tree
+
+	//the code for each possibly representable value
+	// for example, if 44 is the boolean vector 10, then it will have 10 at the 44th index
+	//all valueCodes are initialized to an empty boolean array
+	std::array<std::vector<bool>, NUM_UINT8_VALUES> valueCodes;
+
+	//keep a double-ended queue to traverse the tree, building up the codes for each part of the tree
+	std::deque<std::pair<HuffmanTree<uint8_t> *, std::vector<bool>>> remaining_nodes;
+	remaining_nodes.emplace_back(huffmanTree, std::vector<bool>());
+
+	//while more tree to convert
+	while(!remaining_nodes.empty())
 	{
-		delete left;
-		delete right;
+		auto node = remaining_nodes.front().first;
+		//explicitly make a copy to make sure there's not a reference being kept
+		std::vector<bool> code(remaining_nodes.front().second);
+		remaining_nodes.pop_front();
+
+		auto left = node->left;
+		auto right = node->right;
+
+		//if not leaf node (a Huffman Tree node is either full or not)
+		if(left != nullptr)
+		{
+			//make another copy of the code for the other child node
+			std::vector<bool> code_copy(code);
+
+			//append a 0 for left, 1 for right
+			code.push_back(0);
+			remaining_nodes.emplace_back(left, code);
+			code_copy.push_back(1);
+			remaining_nodes.emplace_back(right, code_copy);
+		}
+		else //leaf node
+		{
+			valueCodes[node->value] = code;
+		}
 	}
 
-	//number of bits per value based on the number of bytes long value_type is
-	static constexpr int bitsPerValue = 8 * sizeof(value_type);
+	//encode the data and store in compressed_data
+	BinaryData compressed_data;
+	//reserve some, probably not enough, but enough to get started
+	compressed_data.reserve(1 + uncompressed_data.size() / 4);
 
-	static HuffmanTree<value_type> *BuildTreeFromValueFrequencies(
-		std::array<value_type, std::numeric_limits<value_type>::max() + 1> &byte_frequencies)
+	//the first byte stores the number of extra bits in the last byte, so skip it for encoding
+	size_t ending_bit = 8;
+	size_t cur_byte = 1;
+	size_t cur_bit = 0;
+
+	for(uint8_t c : uncompressed_data)
 	{
-		size_t cur_node_index = 0;
+		auto &value = valueCodes[c];
+		size_t num_bits_to_add = value.size();
 
-		//start by building the leaf nodes
-		std::priority_queue<HuffmanTree<value_type> *,
-			std::vector<HuffmanTree<value_type> *>, HuffmanTree<value_type>::Compare > alphabet_heap;
+		//make sure there are enough bytes to hold everything
+		// if one extra bit, then need a full extra byte, so add 7 bits to round up
+		ending_bit += num_bits_to_add;
+		compressed_data.resize((ending_bit + 7) / 8);
 
-		//create all the leaf nodes and add them to the priority queue
-		for(size_t i = 0; i < byte_frequencies.size(); i++)
+		for(auto bit : value)
 		{
-			auto leaf = new HuffmanTree<value_type>(static_cast<value_type>(i), byte_frequencies[i], cur_node_index++);
-			alphabet_heap.push(leaf);
-		}
+			//compressed_data is already initialized to zeros, so only need to set if true
+			if(bit)
+				compressed_data[cur_byte] |= (1 << cur_bit);
 
-		//Merge leaf nodes with lowest values until have just one at the top
-		HuffmanTree<value_type> *huffman_tree = nullptr;
-		while(alphabet_heap.size() > 1)
-		{
-			auto left = alphabet_heap.top();
-			alphabet_heap.pop();
-			auto right = alphabet_heap.top();
-			alphabet_heap.pop();
-
-			//since non-leaf nodes aren't used for encoding, just use the value 0
-			huffman_tree = new HuffmanTree<value_type>(0, left->valueFrequency + right->valueFrequency, cur_node_index++, left, right);
-			alphabet_heap.push(huffman_tree);
-		}
-
-		return huffman_tree;
-	}
-
-	//for sorting HuffmanTree nodes by frequency
-	class Compare
-	{
-	public:
-		constexpr bool operator()(HuffmanTree<value_type> *a, HuffmanTree<value_type> *b)
-		{
-			//if valueFrequency is the same for both values, break tie by the value itself
-			// to ensure consistent ordering across platforms and heap implementations
-			if(a->valueFrequency == b->valueFrequency)
-			{
-				//if values are equal, break ties by node index
-				if(a->value == b->value)
-					return a->nodeIndex > b->nodeIndex;
-
-				return a->value > b->value;
-			}
-			return a->valueFrequency > b->valueFrequency;
-		}
-	};
-
-	//looks up the next value in the tree based from the bit string in bd from start_index up until end_index
-	//increments start_index based on the length of the code consumed
-	inline value_type LookUpCode(BinaryData &bd, size_t &start_index, size_t end_index)
-	{
-		auto node = this;
-
-		size_t cur_byte = (start_index / bitsPerValue);
-		size_t cur_bit = (start_index % bitsPerValue);
-
-		while(start_index < end_index)
-		{
-			//if leaf node, then return value
-			if(node->left == nullptr)
-				return node->value;
-
-			if(bd[cur_byte] & (1 << cur_bit) )
-				node = node->right;
-			else
-				node = node->left;
-
-			start_index++;
 			cur_bit++;
-			if(cur_bit == bitsPerValue)
+			if(cur_bit == 8)
 			{
 				cur_bit = 0;
 				cur_byte++;
 			}
 		}
-
-		//if leaf node, then return value; need this again incase used up last bits
-		if(node->left == nullptr)
-			return node->value;
-
-		//shouldn't make it here -- ran out of bits
-		return 0;
 	}
 
-	//the value of this node in the HuffmanTree and its frequency
-	value_type value;
-	size_t valueFrequency;
+	//store number of extra bits in first byte
+	compressed_data[0] = (ending_bit % 8);
 
-	//node index used for breaking ties on value and valueFrequency to ensure
-	// that Huffman trees are always generated identically regardless of priority queue implementation
-	size_t nodeIndex;
+	return compressed_data;
+}
 
-	//rest of the tree
-	HuffmanTree<value_type> *left;
-	HuffmanTree<value_type> *right;
-};
-
-//class to compress and decompress bundles of strings
-class StringCodec
+std::string StringCodec::DecodeString(BinaryData &compressed_data)
 {
-public:
+	//need at least one byte to represent the number of extra bits and another byte of actual value
+	if(compressed_data.size() < 2)
+		return std::string();
 
-	const static size_t NUM_UINT8_VALUES = std::numeric_limits<uint8_t>::max() + 1;
+	//count out all the potentially available bits
+	size_t end_bit = 8 * compressed_data.size();
 
-	StringCodec(std::array<uint8_t, NUM_UINT8_VALUES> &byte_frequencies)
+	//number of extra bits is stored in the first byte
+	if(compressed_data[0] != 0)
 	{
-		//build the huffman_tree based on the byte frequencies
-		huffmanTree = HuffmanTree<uint8_t>::BuildTreeFromValueFrequencies(byte_frequencies);
+		//if there is any number besides 0, then we need to remove 8 bits and add on whatever remains
+		end_bit -= 8;
+		end_bit += compressed_data[0];
+	}
+	//skip the first byte
+	size_t start_bit = 8;
+
+	//decompress the data
+	std::string uncompressed_data;
+	while(start_bit < end_bit)
+		uncompressed_data.push_back(huffmanTree->LookUpCode(compressed_data, start_bit, end_bit));
+
+	return uncompressed_data;
+}
+
+//counts the number of bytes within bd for each value
+//returns an array where each index represents each of the possible NUM_INT8_VALUES values, and the value at each is the number found
+static std::array<uint8_t, StringCodec::NUM_UINT8_VALUES> GetByteFrequencies(std::string &str)
+{
+	std::array<size_t, StringCodec::NUM_UINT8_VALUES> value_counts{};	//initialize to zero with value-initialization {}'s
+	for(uint8_t b : str)
+		value_counts[b]++;
+
+	//get maximal count for any value
+	size_t max_count = 0;
+	for(auto v : value_counts)
+		max_count = std::max(max_count, v);
+
+	std::array<uint8_t, StringCodec::NUM_UINT8_VALUES> normalized_value_counts{};	//initialize to zero with value-initialization {}'s
+	for(size_t i = 0; i < StringCodec::NUM_UINT8_VALUES; i++)
+	{
+		if(value_counts[i] == 0)
+			continue;
+		normalized_value_counts[i] = std::max(static_cast<uint8_t>(255 * value_counts[i] / max_count), static_cast<uint8_t>(1));	//make sure it has at least a value of 1 to be represented
 	}
 
-	~StringCodec()
-	{
-		if(huffmanTree != nullptr)
-			delete huffmanTree;
-	}
-
-	inline BinaryData EncodeString(std::string &uncompressed_data)
-	{
-		//build lookup table from huffman_tree
-
-		//the code for each possibly representable value
-		// for example, if 44 is the boolean vector 10, then it will have 10 at the 44th index
-		//all valueCodes are initialized to an empty boolean array
-		std::array<std::vector<bool>, NUM_UINT8_VALUES> valueCodes;
-
-		//keep a double-ended queue to traverse the tree, building up the codes for each part of the tree
-		std::deque<std::pair<HuffmanTree<uint8_t> *, std::vector<bool>>> remaining_nodes;
-		remaining_nodes.emplace_back(huffmanTree, std::vector<bool>());
-
-		//while more tree to convert
-		while(!remaining_nodes.empty())
-		{
-			auto node = remaining_nodes.front().first;
-			//explicitly make a copy to make sure there's not a reference being kept
-			std::vector<bool> code(remaining_nodes.front().second);
-			remaining_nodes.pop_front();
-
-			auto left = node->left;
-			auto right = node->right;
-
-			//if not leaf node (a Huffman Tree node is either full or not)
-			if(left != nullptr)
-			{
-				//make another copy of the code for the other child node
-				std::vector<bool> code_copy(code);
-
-				//append a 0 for left, 1 for right
-				code.push_back(0);
-				remaining_nodes.emplace_back(left, code);
-				code_copy.push_back(1);
-				remaining_nodes.emplace_back(right, code_copy);
-			}
-			else //leaf node
-			{
-				valueCodes[node->value] = code;
-			}
-		}
-
-		//encode the data and store in compressed_data
-		BinaryData compressed_data;
-		//reserve some, probably not enough, but enough to get started
-		compressed_data.reserve(1 + uncompressed_data.size() / 4);
-
-		//the first byte stores the number of extra bits in the last byte, so skip it for encoding
-		size_t ending_bit = 8;
-		size_t cur_byte = 1;
-		size_t cur_bit = 0;
-
-		for(uint8_t c : uncompressed_data)
-		{
-			auto &value = valueCodes[c];
-			size_t num_bits_to_add = value.size();
-
-			//make sure there are enough bytes to hold everything
-			// if one extra bit, then need a full extra byte, so add 7 bits to round up
-			ending_bit += num_bits_to_add;
-			compressed_data.resize((ending_bit + 7) / 8);
-
-			for(auto bit : value)
-			{
-				//compressed_data is already initialized to zeros, so only need to set if true
-				if(bit)
-					compressed_data[cur_byte] |= (1 << cur_bit);
-
-				cur_bit++;
-				if(cur_bit == 8)
-				{
-					cur_bit = 0;
-					cur_byte++;
-				}
-			}
-		}
-
-		//store number of extra bits in first byte
-		compressed_data[0] = (ending_bit % 8);
-
-		return compressed_data;
-	}
-
-	inline std::string DecodeString(BinaryData &compressed_data)
-	{
-		//need at least one byte to represent the number of extra bits and another byte of actual value
-		if(compressed_data.size() < 2)
-			return std::string();
-
-		//count out all the potentially available bits
-		size_t end_bit = 8 * compressed_data.size();
-
-		//number of extra bits is stored in the first byte
-		if(compressed_data[0] != 0)
-		{
-			//if there is any number besides 0, then we need to remove 8 bits and add on whatever remains
-			end_bit -= 8;
-			end_bit += compressed_data[0];
-		}
-		//skip the first byte
-		size_t start_bit = 8;
-
-		//decompress the data
-		std::string uncompressed_data;
-		while(start_bit < end_bit)
-			uncompressed_data.push_back(huffmanTree->LookUpCode(compressed_data, start_bit, end_bit));
-
-		return uncompressed_data;
-	}
-
-	//counts the number of bytes within bd for each value
-	//returns an array where each index represents each of the possible NUM_INT8_VALUES values, and the value at each is the number found
-	static std::array<uint8_t, NUM_UINT8_VALUES> GetByteFrequencies(std::string &str)
-	{
-		std::array<size_t, NUM_UINT8_VALUES> value_counts{};	//initialize to zero with value-initialization {}'s
-		for(uint8_t b : str)
-			value_counts[b]++;
-
-		//get maximal count for any value
-		size_t max_count = 0;
-		for(auto v : value_counts)
-			max_count = std::max(max_count, v);
-
-		std::array<uint8_t, NUM_UINT8_VALUES> normalized_value_counts{};	//initialize to zero with value-initialization {}'s
-		for(size_t i = 0; i < NUM_UINT8_VALUES; i++)
-		{
-			if(value_counts[i] == 0)
-				continue;
-			normalized_value_counts[i] = std::max(static_cast<uint8_t>(255 * value_counts[i] / max_count), static_cast<uint8_t>(1));	//make sure it has at least a value of 1 to be represented
-		}
-
-		return normalized_value_counts;
-	}
-
-	//Huffman tree to build and store between calls
-	HuffmanTree<uint8_t> *huffmanTree;
-};
+	return normalized_value_counts;
+}
 
 BinaryData CompressString(std::string &string_to_compress)
 {
@@ -339,7 +206,7 @@ BinaryData CompressString(std::string &string_to_compress)
 	encoded_string_library.reserve(2 * StringCodec::NUM_UINT8_VALUES);	//reserve enough to two entries for every value in the worst case; this will be expanded later
 
 	//create and store the frequency table for each possible byte value
-	auto byte_frequencies = StringCodec::GetByteFrequencies(string_to_compress);
+	auto byte_frequencies = GetByteFrequencies(string_to_compress);
 	for(size_t i = 0; i < StringCodec::NUM_UINT8_VALUES; i++)
 	{
 		//write value
--- src/Amalgam/BinaryPacking.h
+++ src/Amalgam/BinaryPacking.h
@@ -6,6 +6,148 @@
 
 typedef std::vector<uint8_t> BinaryData;
 
+//Huffman Encoding implementation for compressing and decompressing data
+template <typename value_type>
+class HuffmanTree
+{
+public:
+	constexpr HuffmanTree(value_type value, size_t value_frequency, size_t node_index,
+		HuffmanTree<value_type> *left = nullptr, HuffmanTree<value_type> *right = nullptr)
+		: value(value), valueFrequency(value_frequency), nodeIndex(node_index), left(left), right(right)
+	{
+	}
+
+	~HuffmanTree()
+	{
+		delete left;
+		delete right;
+	}
+
+	//number of bits per value based on the number of bytes long value_type is
+	static constexpr int bitsPerValue = 8 * sizeof(value_type);
+
+	static HuffmanTree<value_type> *BuildTreeFromValueFrequencies(
+		std::array<value_type, std::numeric_limits<value_type>::max() + 1> &byte_frequencies)
+	{
+		size_t cur_node_index = 0;
+
+		//start by building the leaf nodes
+		std::priority_queue<HuffmanTree<value_type> *,
+			std::vector<HuffmanTree<value_type> *>, HuffmanTree<value_type>::Compare > alphabet_heap;
+
+		//create all the leaf nodes and add them to the priority queue
+		for(size_t i = 0; i < byte_frequencies.size(); i++)
+		{
+			auto leaf = new HuffmanTree<value_type>(static_cast<value_type>(i), byte_frequencies[i], cur_node_index++);
+			alphabet_heap.push(leaf);
+		}
+
+		//Merge leaf nodes with lowest values until have just one at the top
+		HuffmanTree<value_type> *huffman_tree = nullptr;
+		while(alphabet_heap.size() > 1)
+		{
+			auto left = alphabet_heap.top();
+			alphabet_heap.pop();
+			auto right = alphabet_heap.top();
+			alphabet_heap.pop();
+
+			//since non-leaf nodes aren't used for encoding, just use the value 0
+			huffman_tree = new HuffmanTree<value_type>(0, left->valueFrequency + right->valueFrequency, cur_node_index++, left, right);
+			alphabet_heap.push(huffman_tree);
+		}
+
+		return huffman_tree;
+	}
+
+	//for sorting HuffmanTree nodes by frequency
+	class Compare
+	{
+	public:
+		constexpr bool operator()(HuffmanTree<value_type> *a, HuffmanTree<value_type> *b)
+		{
+			//if valueFrequency is the same for both values, break tie by the value itself
+			// to ensure consistent ordering across platforms and heap implementations
+			if(a->valueFrequency == b->valueFrequency)
+			{
+				//if values are equal, break ties by node index
+				if(a->value == b->value)
+					return a->nodeIndex > b->nodeIndex;
+
+				return a->value > b->value;
+			}
+			return a->valueFrequency > b->valueFrequency;
+		}
+	};
+
+	//looks up the next value in the tree based from the bit string in bd from start_index up until end_index
+	//increments start_index based on the length of the code consumed
+	inline value_type LookUpCode(BinaryData &bd, size_t &start_index, size_t end_index)
+	{
+		auto node = this;
+
+		size_t cur_byte = (start_index / bitsPerValue);
+		size_t cur_bit = (start_index % bitsPerValue);
+
+		while(start_index < end_index)
+		{
+			//if leaf node, then return value
+			if(node->left == nullptr)
+				return node->value;
+
+			if(bd[cur_byte] & (1 << cur_bit))
+				node = node->right;
+			else
+				node = node->left;
+
+			start_index++;
+			cur_bit++;
+			if(cur_bit == bitsPerValue)
+			{
+				cur_bit = 0;
+				cur_byte++;
+			}
+		}
+
+		//if leaf node, then return value; need this again incase used up last bits
+		if(node->left == nullptr)
+			return node->value;
+
+		//shouldn't make it here -- ran out of bits
+		return 0;
+	}
+
+	//the value of this node in the HuffmanTree and its frequency
+	value_type value;
+	size_t valueFrequency;
+
+	//node index used for breaking ties on value and valueFrequency to ensure
+	// that Huffman trees are always generated identically regardless of priority queue implementation
+	size_t nodeIndex;
+
+	//rest of the tree
+	HuffmanTree<value_type> *left;
+	HuffmanTree<value_type> *right;
+};
+
+//class to compress and decompress bundles of strings
+class StringCodec
+{
+public:
+
+	const static size_t NUM_UINT8_VALUES = std::numeric_limits<uint8_t>::max() + 1;
+
+	StringCodec(std::array<uint8_t, NUM_UINT8_VALUES> &byte_frequencies);
+
+	~StringCodec();
+
+	BinaryData EncodeString(std::string &uncompressed_data);
+
+	std::string DecodeString(BinaryData &compressed_data);
+
+	//Huffman tree to build and store between calls
+	HuffmanTree<uint8_t> *huffmanTree;
+};
+
 //given string_map, map of string to index, where the indices are of the range from 0 to string_map.size(), compresses the strings into BinaryData
 BinaryData CompressString(std::string &string_to_compress);
 

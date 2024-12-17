1e62b193a306a56ac252941b5cfa81a37f44ab1e
Nathan Ricci <naricc@howso.com>
2024-12-16 20:11:14 -0500

    Updated docs.

--- docs/language.js
+++ docs/language.js
@@ -507,17 +507,17 @@ var data = [
 		"example" : "(print (min 0.5 1 7 9 -5))"
 	},
 	{
-		"parameter" : "index_max *",
+		"parameter" : "index_max [number x1] [number x2] [number x3] ... [number xN] | (assoc ...) | (list ...)",
 		"output" : "*",
 		"concurrency" : true,
-		"description" : "the list index or assoc key mapped to the maximum value",
+		"description" : "the index of of the argument with the greatest value, or the key associated with the greatest value in an assoc, or the index of the greatest value in a list",
 		"example" : "(print (index_max 0.5 1 7 9 -5))\n(print (index_max (list 0.5 1 7 9 -5) ))\n(print (index_max (assoc 1 0.5 2 1 3 7)))"
 	},
 	{
-		"parameter" : "index_min *",
+		"parameter" : "index_min [number x1] [number x2] [number x3] ... [number xN] | (assoc ...) | (list ...)",
 		"output" : "*",
 		"concurrency" : true,
-		"description" : "the list index or assoc key mapped to the minimum value",
+		"description" : "the index of of the argument with the least value, or the key associated with the least value in an assoc, or the index of the least value in a list",
 		"example" : "(print (index_min 0.5 1 7 9 -5))\n(print (index_min (list 0.5 1 7 9 -5) ))\n(print (index_min (assoc 1 0.5 2 1 3 7)))"
 	},
 	{

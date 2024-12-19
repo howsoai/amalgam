24d2abee9fc85b387b9e7989a3884d64b8824669
Nathan Ricci <naricc@howso.com>
2024-12-19 18:04:49 -0500

    Updated documentation.

--- docs/language.js
+++ docs/language.js
@@ -510,14 +510,14 @@ var data = [
 		"parameter" : "index_max [number x1] [number x2] [number x3] ... [number xN] | (assoc ...) | (list ...)",
 		"output" : "[*]",
 		"concurrency" : true,
-		"description" : "a list of the indices of the argument with the greatest value, or the keys associated with the greatest value in an assoc, or the indices of the greatest value in a list",
+		"description" : "If given multiple arguments, returns a list of the indices of the arguments with the maximum value. If given a single argument that is an assoc returns the a list of keys associated with the maximum values. If given a single argument that is a list, returns a list of list indices with the maximum value.",
 		"example" : "(print (index_max 0.5 1 7 9 -5))\n(print (index_max (list 0.5 1 7 9 -5) ))\n(print (index_max (assoc 1 0.5 2 1 3 7)))"
 	},
 	{
 		"parameter" : "index_min [number x1] [number x2] [number x3] ... [number xN] | (assoc ...) | (list ...)",
 		"output" : "[*]",
 		"concurrency" : true,
-		"description" : "a list of the indices of of the arguments with the least value, or the keys associated with the least value in an assoc, or the indices of the least value in a list",
+		"description" : "If given multiple arguments, returns a list of the indices of the arguments with the minimum value. If given a single argument that is an assoc returns the a list of keys associated with the minimum values. If given a single argument that is a list, returns a list of list indices with the minimum value.",
 		"example" : "(print (index_min 0.5 1 7 9 -5))\n(print (index_min (list 0.5 1 7 9 -5) ))\n(print (index_min (assoc 1 0.5 2 1 3 7)))"
 	},
 	{

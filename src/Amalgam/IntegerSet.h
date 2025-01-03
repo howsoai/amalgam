fd26732e9e08905d50db6fb9785a93331fe87066
Christopher J. Hazard, PhD <143410553+howsohazard@users.noreply.github.com>
2025-01-02 20:19:57 -0500

    22454: Fixes GCC issue

--- src/Amalgam/IntegerSet.h
+++ src/Amalgam/IntegerSet.h
@@ -434,7 +434,7 @@ public:
 			//calculate last bucket in case less than total size
 			size_t end_buckets = (end_index + 63) / 64;
 			size_t index = 0;
-			for(size_t bucket = 0, index = 0; bucket < end_buckets; bucket++)
+			for(size_t bucket = 0; bucket < end_buckets; bucket++)
 			{
 				uint64_t bucket_bits = bitBucket[bucket];
 				for(size_t bit = 0; bit < 64; bit++, index++)

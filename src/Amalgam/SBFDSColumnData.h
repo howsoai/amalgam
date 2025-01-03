58f6c7b706d30c19e303dc916dd6609cb1d65e2b
Christopher J. Hazard, PhD <143410553+howsohazard@users.noreply.github.com>
2025-01-02 21:40:20 -0500

    22454: GCC

--- src/Amalgam/SBFDSColumnData.h
+++ src/Amalgam/SBFDSColumnData.h
@@ -75,7 +75,8 @@ public:
 		{
 			numberIndices.insert(index);
 
-			auto [value_entry_iter, inserted] = sortedNumberValueEntries.emplace(value.number, value.number);
+			std::pair<const double, ValueEntry> pair_to_insert(value.number, value.number);
+			auto [value_entry_iter, inserted] = sortedNumberValueEntries.insert(std::move(pair_to_insert));
 			value_entry_iter->second.indicesWithValue.InsertNewLargestInteger(index);
 		}
 		else if(value_type == ENIVT_STRING_ID)
@@ -210,7 +211,8 @@ public:
 				//if the value already exists, then put the index in the list
 				//but return the lower bound if not found so don't have to search a second time
 				//need to search the old value before inserting, as FindExactIndexForValue is fragile a placeholder empty entry
-				auto [new_value_entry_iter, inserted] = sortedNumberValueEntries.emplace(new_number_value, new_number_value);
+				std::pair<const double, ValueEntry> pair_to_insert(new_number_value, new_number_value);
+				auto [new_value_entry_iter, inserted] = sortedNumberValueEntries.insert(std::move(pair_to_insert));
 				auto &new_value_entry = new_value_entry_iter->second;
 				auto old_value_entry = sortedNumberValueEntries.find(old_number_value);
 
@@ -559,7 +561,8 @@ public:
 
 			double number_value = GetResolvedValue(value_type, value).number;
 
-			auto [value_entry_iter, inserted] = sortedNumberValueEntries.emplace(number_value, number_value);
+			std::pair<const double, ValueEntry> pair_to_insert(number_value, number_value);
+			auto [value_entry_iter, inserted] = sortedNumberValueEntries.insert(std::move(pair_to_insert));
 			auto &value_entry = value_entry_iter->second;
 			if(!inserted)
 			{

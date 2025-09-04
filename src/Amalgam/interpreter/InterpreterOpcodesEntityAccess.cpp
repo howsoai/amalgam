f46364edca5d70b1c066bceba2165d533e273b4a
howsohazard <143410553+howsohazard@users.noreply.github.com>
2025-09-04 19:28:50 -0400

    24158: Improves thread memory retention and fixes two potential issues that could lead to instability (#437)

--- src/Amalgam/interpreter/InterpreterOpcodesEntityAccess.cpp
+++ src/Amalgam/interpreter/InterpreterOpcodesEntityAccess.cpp
@@ -278,15 +278,15 @@ EvaluableNodeReference Interpreter::InterpretNode_ENT_ASSIGN_TO_ENTITIES_and_DIR
 		bool copy_entity = false;
 
 		//pause if allocating to another entity
-		EvaluableNodeManager::ThreadLocalAllocationBufferPause tlab_pause;
+		EvaluableNodeManager::LocalAllocationBufferPause lab_pause;
 		if(target_entity != curEntity)
-			tlab_pause = evaluableNodeManager->PauseThreadLocalAllocationBuffer();
+			lab_pause = evaluableNodeManager->PauseLocalAllocationBuffer();
 
 		auto [any_success, all_success] = target_entity->SetValuesAtLabels(
 										assigned_vars, accum_assignment, direct, writeListeners,
 										(ConstrainedAllocatedNodes() ? &num_new_nodes_allocated : nullptr), target_entity == curEntity, copy_entity);
 
-		tlab_pause.Resume();
+		lab_pause.Resume();
 
 		if(any_success)
 		{
@@ -311,11 +311,12 @@ EvaluableNodeReference Interpreter::InterpretNode_ENT_ASSIGN_TO_ENTITIES_and_DIR
 			#endif
 			}
 		}
-		//clear write lock as soon as possible
+		//clear write lock as soon as possible, but pull out pointer first to compare for gc
+		Entity *target_entity_raw_ptr = target_entity;
 		target_entity = EntityWriteReference();
 
 		//if assigning to a different entity, it can be cleared
-		if(target_entity != curEntity)
+		if(target_entity_raw_ptr != curEntity)
 		{
 			node_stack.PopEvaluableNode();
 			evaluableNodeManager->FreeNodeTreeIfPossible(assigned_vars);

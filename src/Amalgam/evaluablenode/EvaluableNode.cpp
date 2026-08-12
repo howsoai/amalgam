4c28ac1eb874385eb341ab1e5ca9ada669314ddb
Christopher J. Hazard, PhD <143410553+howsohazard@users.noreply.github.com>
2026-08-12 13:41:16 -0400

    15994: More progress

--- src/Amalgam/evaluablenode/EvaluableNode.cpp
+++ src/Amalgam/evaluablenode/EvaluableNode.cpp
@@ -130,7 +130,7 @@ double EvaluableNode::ToNumber(EvaluableNode *e, double value_if_null)
 		if(sid == string_intern_pool.NOT_A_STRING_ID)
 			return value_if_null;
 		auto str = string_intern_pool.GetStringViewFromID(sid);
-		auto [value, success] = Platform_StringToNumber(str);
+		auto [value, success] = StringManipulation::StringToNumber(str);
 		if(success)
 			return value;
 		return value_if_null;
@@ -1485,7 +1485,7 @@ double EvaluableNodeImmediateValueWithType::GetValueAsNumber(double value_if_nul
 			return value_if_null;
 
 		auto str = string_intern_pool.GetStringViewFromID(nodeValue.stringID);
-		auto [value, success] = Platform_StringToNumber(str);
+		auto [value, success] = StringManipulation::StringToNumber(str);
 		if(success)
 			return value;
 		return value_if_null;

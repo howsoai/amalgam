fc0381c58d613c123cae9dfd396d5fa6edf00e4c
Christopher J. Hazard, PhD <143410553+howsohazard@users.noreply.github.com>
2026-09-02 20:15:36 -0400

    25910: Fixes most unit tests

--- src/Amalgam/interpreter/OpcodesEntityQueryEngine.cpp
+++ src/Amalgam/interpreter/OpcodesEntityQueryEngine.cpp
@@ -1593,13 +1593,13 @@ static OpcodeInitializer _ENT_QUERY_NEAREST_GENERALIZED_DISTANCE(ENT_QUERY_NEARE
 		)
 	]
 ))&", R"([
-	{vert2 1 vert3 1.4142135623730951 vert5 1.4142135623730951}
-	{
-		vert2 1
-		vert3 1.4142135623730951
-		vert4 1.5811388300841898
-		vert5 1.4142135623730951
-	}
+		{vert2 1 vert3 1.4142135623730951 vert5 1.4142135623730951}
+		{
+				vert2 1
+				vert5 1.4142135623730951
+				vert3 1.4142135623730951
+				vert4 1.5811388300841898
+		}
 ])", "", R"((apply "destroy_entities" (contained_entities)))"},
 {R"&((seq
 	(create_entities
@@ -1663,7 +1663,7 @@ static OpcodeInitializer _ENT_QUERY_NEAREST_GENERALIZED_DISTANCE(ENT_QUERY_NEARE
 			"random seed 1234"
 		)
 	)
-))&", R"({vert0 3 vert1 2 vert4 3.5})", "", R"((apply "destroy_entities" (contained_entities)))"}
+))&", R"({vert1 2 vert0 3 vert4 3.5})", "", R"((apply "destroy_entities" (contained_entities)))"}
 		});
 	d.valueNewness = OpcodeDetails::OpcodeReturnNewnessType::PARTIAL;
 	d.isQuery = true;

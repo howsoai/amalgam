94e832ba56a35cfff1fd3cb923a13b831ce1335e
Christopher J. Hazard, PhD <143410553+howsohazard@users.noreply.github.com>
2026-09-10 19:59:07 -0400

    25910: Updates doc

--- src/Amalgam/interpreter/OpcodesContainerManipulation.cpp
+++ src/Amalgam/interpreter/OpcodesContainerManipulation.cpp
@@ -589,7 +589,7 @@ static OpcodeInitializer _ENT_TRUNC(ENT_TRUNC, &Interpreter::InterpretNode_ENT_T
 		OpcodeDetails::ParameterGroup({"retain_count", OpcodeDetails::DataType::NUMBER, true})
 	};
 	d.returns = OpcodeDetails::DataType::LIST;
-	d.description = R"(Truncates, evaluates to everything in `node` but the last element. If `node` is a list, it will be a list of all but the last element.  If `node` is an assoc, it will evaluate to the assoc without the first element insertion order.  If `node` is a string, it will be all but the last character.  If `node` is a number, it will evaluate to the value minus 1 if nonzero, 0 if zero. If `truncate_count` is specified, it will be the number of elements to retain.  A positive number means from the beginning, a negative number means from the end.  The default value is -1, indicating all but the last.)";
+	d.description = R"(Truncates, evaluates to everything in `node` but the last element. If `node` is a list, it will be a list of all but the last element.  If `node` is an assoc, it will evaluate to the assoc without the last element by insertion order.  If `node` is a string, it will be all but the last character.  If `node` is a number, it will evaluate to the value minus 1 if nonzero, 0 if zero. If `truncate_count` is specified, it will be the number of elements to retain.  A positive number means from the beginning, a negative number means from the end.  The default value is -1, indicating all but the last.)";
 	d.examples = MakeAmalgamExamples({
 		{R"&((trunc
 	[4 9.2 "end"]
